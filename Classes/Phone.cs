f0d4eaef91561b12db564f988ebd1988cb418b75
romvasko <romvasko@yandex.by>
2022-12-13 10:16:36 +0300

    first commit

---
+++ Classes/Phone.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Classes {
+    internal class Phone {
+        public int number { get; set; }
+        public string model { get; set; }
+        public int weight { get; set; }
+        public Phone(int num, string mod, int wei) 
+        {
+            number = num;
+            model = mod;
+            weight = wei;
+        }
+        public override string ToString() {
+            return number.ToString() + " " + model.ToString() + " " + weight.ToString();
+        }
+        public string ReceiveCall(string name)
+        {
+            return name + " is calling";
+        }
+        public string ReceiveCall(string name, int num)
+        {
+            return name + " is calling " + num.ToString("+375 (44) ###-##-##");
+        }
+        public string GetNumber() {
+            return number.ToString("+375 (44) ###-##-##");
+        }
+        public static string SendMessage(params int[] num) {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Phone numbers to send message:\n");
+            foreach (int n in num) {
+                sb.Append(n.ToString("+375 (44) ###-##-##\n"));
+            }
+            return sb.ToString();
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+using Lab3.Classes;
+
+namespace Lab3 
+{
+    internal class Program {
+        static void Main(string[] args) {
+            List<Phone> phoneList = new List<Phone>();
+            phoneList.Add(new Phone(7777777, "Nokia", 120));
+            phoneList.Add(new Phone(5555555, "Sony", 200));
+            phoneList.Add(new Phone(3333333, "Samsung", 240));
+           foreach (var ph in phoneList) {
+                Console.WriteLine(ph.ToString());
+                Console.WriteLine(ph.GetNumber());
+                Console.WriteLine(ph.ReceiveCall("John"));
+                Console.WriteLine(ph.ReceiveCall("Arlo", 1111111));
+                Console.WriteLine();
+            }
+            Console.WriteLine(Phone.SendMessage(1000000, 4561223, 7897889)); 
+        }
+   
+
+	}
+
+}

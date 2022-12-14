477de3148b56f59f9dee3d2b9c64c5e90e0316be
romvasko <romvasko@yandex.by>
2022-12-14 22:08:45 +0300

    prac

---
+++ Classes/CreditCard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Classes
+{
+    internal class CreditCard
+    {
+        string number;
+        int cash;
+        public CreditCard(string number, int cash)
+        {
+            this.number = number;
+            this.cash = cash;
+        }
+        public int AddCash(int sum)
+        {
+            cash += sum;
+            return 0;
+        }
+       public int GetCash(int sum)
+        {
+            cash -= sum;
+            return 0;
+        }
+        public override string ToString()
+        {
+            return "Credit card number: " + number + " Cash on card: " + cash.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,18 +9,32 @@ namespace Lab3
 {
     internal class Program {
         static void Main(string[] args) {
-            List<Phone> phoneList = new List<Phone>();
-            phoneList.Add(new Phone(7777777, "Nokia", 120));
-            phoneList.Add(new Phone(5555555, "Sony", 200));
-            phoneList.Add(new Phone(3333333, "Samsung", 240));
-           foreach (var ph in phoneList) {
-                Console.WriteLine(ph.ToString());
-                Console.WriteLine(ph.GetNumber());
-                Console.WriteLine(ph.ReceiveCall("John"));
-                Console.WriteLine(ph.ReceiveCall("Arlo", 1111111));
-                Console.WriteLine();
+            List<CreditCard> creditCards = new List<CreditCard>();
+            creditCards.Add(new CreditCard("111",200));
+            creditCards.Add(new CreditCard("222",200));
+            creditCards.Add(new CreditCard("333",999));
+            creditCards[0].AddCash(10);
+            creditCards[1].AddCash(20);
+            creditCards[2].GetCash(999);
+            foreach (var item in creditCards)
+            {
+                Console.WriteLine(item); 
             }
-            Console.WriteLine(Phone.SendMessage(1000000, 4561223, 7897889)); 
+
+
+
+            // List<Phone> phoneList = new List<Phone>();
+            // phoneList.Add(new Phone(7777777, "Nokia", 120));
+            // phoneList.Add(new Phone(5555555, "Sony", 200));
+            // phoneList.Add(new Phone(3333333, "Samsung", 240));
+            //foreach (var ph in phoneList) {
+            //     Console.WriteLine(ph.ToString());
+            //     Console.WriteLine(ph.GetNumber());
+            //     Console.WriteLine(ph.ReceiveCall("John"));
+            //     Console.WriteLine(ph.ReceiveCall("Arlo", 1111111));
+            //     Console.WriteLine();
+            // }
+            // Console.WriteLine(Phone.SendMessage(1000000, 4561223, 7897889)); 
         }
    
 
